b526da0fd41d8b16da95753ab54ff5a7e995cf17
Mohamed Said <themohamedsaid@gmail.com>
2020-06-09 13:26:49 +0200

    add a tinker command

---
+++ src/Commands/TinkerCommand.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Laravel\VaporCli\Commands;
+
+use Laravel\VaporCli\Helpers;
+use Laravel\VaporCli\Manifest;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Input\InputArgument;
+
+class TinkerCommand extends CommandCommand
+{
+    /**
+     * Configure the command options.
+     *
+     * @return void
+     */
+    protected function configure()
+    {
+        $this
+            ->setName('tinker')
+            ->addArgument('environment', InputArgument::OPTIONAL, 'The environment name', 'staging')
+            ->addOption('code', null, InputOption::VALUE_OPTIONAL, 'The code to execute with Tinker')
+            ->setDescription('Execute a code with Tinker');
+    }
+
+    /**
+     * Get the command to run.
+     *
+     * @return string
+     */
+    protected function getCommand()
+    {
+        $code = $this->option('code') ?? Helpers::ask('What code would you like to execute');
+
+        return 'tinker --execute "'.$code.'"';
+    }
+}
