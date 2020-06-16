4680e342e60c333491370c6793009564ac67abfb
Taylor Otwell <taylorotwell@gmail.com>
2020-06-16 12:01:03 -0500

    formatting

--- src/Commands/TeamSwitchCommand.php
+++ src/Commands/TeamSwitchCommand.php
@@ -18,8 +18,7 @@ class TeamSwitchCommand extends Command
         $this
             ->setName('team:switch')
             ->setAliases(['switch'])
-            ->addOption('name', null, InputOption::VALUE_OPTIONAL, 'Team Name to switch to')
-            ->addOption('id', null, InputOption::VALUE_OPTIONAL, 'Team ID to switch to')
+            ->addOption('id', null, InputOption::VALUE_OPTIONAL, 'The ID of the team to switch to')
             ->setDescription('Switch to a different team context, you may optionally pass Team ID or Team Name');
     }
 
@@ -37,27 +36,19 @@ class TeamSwitchCommand extends Command
             $this->vapor->teams()
         );
 
-        if( ! empty($this->option('name')) &&  ! empty($this->option('id')))
-        {
-            Helpers::abort('Use either Team Name or Team ID to switch your team.');
-        }
+        $teamId = null;
 
-        if( ! empty($this->option('name')) ||  ! empty($this->option('id')))
-        {
-            $searchBy = ! empty($this->option('name')) ? 'name' : 'id';
+        if (! empty($this->option('id'))) {
+            $team = collect($allTeams)->where('id', $this->option('id'))->first();
 
-            $team = collect($allTeams)->where($searchBy, $this->option($searchBy))->first();
-
-            if(empty($team))
-            {
+            if (empty($team)) {
                 Helpers::abort('Team not found.');
             }
 
             $teamId = $team['id'];
         }
 
-        if( ! isset($teamId))
-        {
+        if (is_null($teamId)) {
             $teamId = $this->menu(
                 'Which team would you like to switch to?',
                 collect($allTeams)->sortBy->name->mapWithKeys(function ($team) {
