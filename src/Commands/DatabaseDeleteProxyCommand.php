2ed7740d0231a24244e294815969f9949d692c8e
Nuno Maduro <enunomaduro@gmail.com>
2020-10-05 17:12:51 +0200

    wip

---
+++ src/Commands/DatabaseDeleteProxyCommand.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Laravel\VaporCli\Commands;
+
+use Laravel\VaporCli\Helpers;
+use Symfony\Component\Console\Input\InputArgument;
+
+class DatabaseDeleteProxyCommand extends Command
+{
+    /**
+     * Configure the command options.
+     *
+     * @return void
+     */
+    protected function configure()
+    {
+        $this
+            ->setName('database:delete-proxy')
+            ->addArgument('database', InputArgument::REQUIRED, 'The name of the database')
+            ->setDescription('Delete the proxy associated to the database.');
+    }
+
+    /**
+     * Execute the command.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        Helpers::ensure_api_token_is_available();
+
+        $databases = $this->vapor->databases();
+
+        if (!is_numeric($databaseId = $this->argument('database'))) {
+            $databaseId = $this->findIdByName($databases, $databaseId);
+        }
+
+        if (is_null($databaseId)) {
+            Helpers::abort('Unable to find a database with that name / ID.');
+        }
+
+        $response = $this->vapor->deleteDatabaseProxy(
+            $databaseId
+        );
+
+        Helpers::info('Database proxy deleted successfully.');
+    }
+}
---
+++ src/Commands/DatabaseProxyCommand.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Laravel\VaporCli\Commands;
+
+use Laravel\VaporCli\Helpers;
+use Symfony\Component\Console\Input\InputArgument;
+
+class DatabaseProxyCommand extends Command
+{
+    /**
+     * Configure the command options.
+     *
+     * @return void
+     */
+    protected function configure()
+    {
+        $this
+            ->setName('database:proxy')
+            ->addArgument('database', InputArgument::REQUIRED, 'The name of the database')
+            ->setDescription('Create an proxy for a database');
+    }
+
+    /**
+     * Execute the command.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        Helpers::ensure_api_token_is_available();
+
+        $databases = $this->vapor->databases();
+
+        if (!is_numeric($databaseId = $this->argument('database'))) {
+            $databaseId = $this->findIdByName($databases, $databaseId);
+        }
+
+        if (is_null($databaseId)) {
+            Helpers::abort('Unable to find a database with that name / ID.');
+        }
+
+        $response = $this->vapor->createDatabaseProxy(
+            $databaseId
+        );
+
+        Helpers::info('Database proxy created successfully.');
+    }
+}
