477c2f175e4fdd27146df7ca45cb315edb199cf2
Nuno Maduro <enunomaduro@gmail.com>
2020-10-28 18:08:18 +0100

    Adds more database instance classes

--- src/DatabaseInstanceClasses.php
+++ src/DatabaseInstanceClasses.php
@@ -27,17 +27,21 @@ class DatabaseInstanceClasses
             // 'db.t2.medium' => 'db.t2.medium - (2 VCPU, 4Gib RAM) - ~$50 / month',
             // 'db.t2.large' => 'db.t2.large - (2 VCPU, 8Gib RAM) - ~$100 / month',
 
-            'db.t3.micro'  => 'db.t3.micro - (1 VCPU, 1Gib RAM) - ~$15 / month',
-            'db.t3.small'  => 'db.t3.small - (1 VCPU, 2Gib RAM) - ~$25 / month',
+            'db.t3.micro'  => 'db.t3.micro - (2 VCPU, 1Gib RAM) - ~$15 / month',
+            'db.t3.small'  => 'db.t3.small - (2 VCPU, 2Gib RAM) - ~$25 / month',
             'db.t3.medium' => 'db.t3.medium - (2 VCPU, 4Gib RAM) - ~$50 / month',
             'db.t3.large'  => 'db.t3.large - (2 VCPU, 8Gib RAM) - ~$100 / month',
+            'db.t3.xlarge' => 'db.t3.xlarge - (4 VCPU, 16Gib RAM) - ~$200 / month',
+            'db.t3.2xlarge' => 'db.t3.2xlarge - (8 VCPU, 32Gib RAM) - ~$400 / month',
 
             'db.m5.large'    => 'db.m5.large - (2 VCPU, 8GB RAM) - ~$125 / month',
             'db.m5.xlarge'   => 'db.m5.xlarge - (4 VCPU, 16Gib RAM) - ~$250 / month',
             'db.m5.2xlarge'  => 'db.m5.2xlarge - (8 VCPU, 32Gib RAM) - ~$500 / month',
-            'db.m5.4xlarge'  => 'db.m5.4xlarge - (16 VCPU, 64Gib RAM) - ~$1100 / month',
-            'db.m5.12xlarge' => 'db.m5.12xlarge - (48 VCPU, 192GiB RAM) - ~$3300 / month',
-            'db.m5.24xlarge' => 'db.m5.24xlarge - (96 VCPU, 384GiB RAM) - ~$6600 / month',
+            'db.m5.4xlarge'  => 'db.m5.4xlarge - (16 VCPU, 64Gib RAM) - ~$1000 / month',
+            'db.m5.8xlarge'  => 'db.m5.8xlarge - (32 VCPU, 128Gib RAM) - ~$2000 / month',
+            'db.m5.12xlarge' => 'db.m5.12xlarge - (48 VCPU, 192GiB RAM) - ~$3000 / month',
+            'db.m5.16xlarge' => 'db.m5.16xlarge - (64 VCPU, 256Gib RAM) - ~$4000 / month',
+            'db.m5.24xlarge' => 'db.m5.24xlarge - (96 VCPU, 384GiB RAM) - ~$6000 / month',
         ];
     }
 
@@ -53,7 +57,9 @@ class DatabaseInstanceClasses
             'db.r5.xlarge'   => 'db.r5.xlarge - (4 VCPU, 32Gib RAM) - ~$346 / month',
             'db.r5.2xlarge'  => 'db.r5.2xlarge - (8 VCPU, 64Gib RAM) - ~$691 / month',
             'db.r5.4xlarge'  => 'db.r5.4xlarge - (16 VCPU, 128Gib RAM) - ~$1382 / month',
+            'db.r5.8xlarge'  => 'db.r5.8xlarge - (32 VCPU, 256Gib RAM) - ~$2765 / month',
             'db.r5.12xlarge' => 'db.r5.12xlarge - (48 VCPU, 384Gib RAM) - ~$4147 / month',
+            'db.r5.16xlarge' => 'db.r5.16xlarge - (64 VCPU, 512Gib RAM) - ~$5530 / month',
             'db.r5.24xlarge' => 'db.r5.24xlarge - (96 VCPU, 768Gib RAM) - ~$8294 / month',
         ];
     }
