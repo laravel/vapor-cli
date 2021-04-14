7cacc91c03b9b1b4d892989fcaffa009754327f2
Nuno Maduro <enunomaduro@gmail.com>
2021-04-14 11:28:30 +0100

    Adds "ap-northeast-3" region

--- src/Regions.php
+++ src/Regions.php
@@ -19,7 +19,7 @@ class Regions
             'af-south-1' => 'Africa (Capetown) (af-south-1)',
             'ap-east-1'  => 'Asia Pacific (Hong Kong) (ap-east-1)',
             'ap-south-1' => 'Asia Pacific (Mumbai) (ap-south-1)',
-            // 'ap-northeast-3' => 'Asia Pacific (Osaka-Local) (ap-northeast-3)',
+            'ap-northeast-3' => 'Asia Pacific (Osaka) (ap-northeast-3)',
             'ap-northeast-2' => 'Asia Pacific (Seoul) (ap-northeast-2)',
             'ap-southeast-1' => 'Asia Pacific (Singapore) (ap-southeast-1)',
             'ap-southeast-2' => 'Asia Pacific (Sydney) (ap-southeast-2)',
