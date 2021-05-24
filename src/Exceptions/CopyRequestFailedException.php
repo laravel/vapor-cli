58155ca0fa27c1fce810f95c66242e2c91b24dc6
Mohamed Said <themohamedsaid@gmail.com>
2021-05-24 18:21:58 +0200

    fix style

--- src/Exceptions/CopyRequestFailedException.php
+++ src/Exceptions/CopyRequestFailedException.php
@@ -3,14 +3,13 @@
 namespace Laravel\VaporCli\Exceptions;
 
 use RuntimeException;
-use Throwable;
 
 class CopyRequestFailedException extends RuntimeException
 {
     /**
      * The file index.
      *
-     * @var integer
+     * @var int
      */
     public $index;
 
@@ -18,9 +17,9 @@ class CopyRequestFailedException extends RuntimeException
      * Create a new exception instance.
      *
      * @param  string  $message
-     * @param  integer  $index
+     * @param  int  $index
      */
-    public function __construct($message = "", $index = 0)
+    public function __construct($message = '', $index = 0)
     {
         parent::__construct($message);
         $this->index = $index;
@@ -29,7 +28,7 @@ class CopyRequestFailedException extends RuntimeException
     /**
      * Get the file index.
      *
-     * @return integer
+     * @return int
      */
     public function getIndex()
     {
