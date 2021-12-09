afe4aaa273b7c74be5fbec14a1abc9b2c4a70e14
Nuno Maduro <enunomaduro@gmail.com>
2021-12-09 13:08:02 +0000

    Uses PHP 8.1 by default (#159)

--- src/Dockerfile.php
+++ src/Dockerfile.php
@@ -13,7 +13,7 @@ class Dockerfile
     public static function fresh($environment)
     {
         $content = <<<'Dockerfile'
-FROM laravelphp/vapor:php80
+FROM laravelphp/vapor:php81
 
 COPY . /var/task
 Dockerfile;
