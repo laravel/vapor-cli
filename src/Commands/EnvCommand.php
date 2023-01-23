c7e74377aac522ae9525fe47c0383b349e992b3a
Nuno Maduro <enunomaduro@gmail.com>
2023-01-23 02:23:21 +0000

    Uses PHP 8.2 by default (#210)

--- src/Commands/EnvCommand.php
+++ src/Commands/EnvCommand.php
@@ -46,7 +46,7 @@ class EnvCommand extends Command
         Manifest::addEnvironment($environment, [
             'memory'     => 1024,
             'cli-memory' => 512,
-            'runtime'    => $this->option('docker') ? 'docker' : 'php-8.1:al2',
+            'runtime'    => $this->option('docker') ? 'docker' : 'php-8.2:al2',
             'build'      => [
                 'COMPOSER_MIRROR_PATH_REPOS=1 composer install --no-dev',
                 'php artisan event:cache',
