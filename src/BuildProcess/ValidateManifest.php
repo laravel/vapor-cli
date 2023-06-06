fe32069a704563bfc5cd4985d3ec423ffca9faea
Taylor Otwell <taylor@laravel.com>
2023-06-06 08:39:21 -0500

    Update ValidateManifest.php

--- src/BuildProcess/ValidateManifest.php
+++ src/BuildProcess/ValidateManifest.php
@@ -42,7 +42,7 @@ class ValidateManifest
             'php-8.0',
         ])) {
             Helpers::warn(
-                'The runtimes "php-7.3", "php-7.4", and "php-8.0" are deprecated and support will be fully removed from Vapor on 31 Dec 2023.'
+                'The runtimes "php-7.3", "php-7.4", and "php-8.0" are deprecated and support will be fully removed from Vapor on December 31st, 2023.'
                 .' Those runtimes are based on Amazon Linux 1, for which AWS standard support has ended.'
                 .' Amazon Linux 1 is only receiving critical and important security updates and it may not work with new Vapor/AWS features.'
                 .' Please use Amazon Linux 2 with "php-7.4:al2" or "php-8.0:al2" instead.'
@@ -54,7 +54,7 @@ class ValidateManifest
             'php-7.4:imagick',
         ])) {
             Helpers::warn(
-                'The runtimes "php-7.4:al2", and "php-7.4:imagick" are deprecated and support will be fully removed from Vapor on 31 Dec 2023.'
+                'The runtimes "php-7.4:al2", and "php-7.4:imagick" are deprecated and support will be fully removed from Vapor on December 31st, 2023.'
                 .' PHP 7.4 is no longer being maintained or receiving security updates.'
                 .' For a full list of supported runtimes, please see: https://docs.vapor.build/1.0/projects/environments.html#runtime'
             );
@@ -63,7 +63,7 @@ class ValidateManifest
         if (Manifest::runtime($this->environment) === 'php-8.0:al2') {
             Helpers::warn(
                 'The runtime "php-8.0:al2" will be deprecated on 26th November 2023 when PHP 8.0 stops receiving security updates.'
-                .' From this date, it will not be possible to deploy new environments using this runtime and support will be removed for existing runtimes on 26 Feb 2024.'
+                .' From this date, it will not be possible to deploy new environments using this runtime and support will be removed for existing runtimes on February 26, 2024.'
                 .' For a full list of supported runtimes, please see: https://docs.vapor.build/1.0/projects/environments.html#runtime'
             );
         }
