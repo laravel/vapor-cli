d0030df897e9597950b8041b903b33094bc0b57e
Joe Dixon <joedixon@users.noreply.github.com>
2024-04-19 18:51:02 +0100

    [1.x] Fixes CLI storage path for Laravel 11 (#253)
    * laravel 11 artisan replacements
    
    * Apply fixes from StyleCI
    
    ---------
    
    Co-authored-by: StyleCI Bot <bot@styleci.io>

--- src/BuildProcess/ConfigureArtisan.php
+++ src/BuildProcess/ConfigureArtisan.php
@@ -37,11 +37,25 @@ class ConfigureArtisan
                 '<?php',
                 "\$app = require_once __DIR__.'/bootstrap/app.php';",
                 "require __DIR__.'/vendor/autoload.php';",
+                <<<'EOF'
+
+// Bootstrap Laravel and handle the command...
+$status = (require_once __DIR__.'/bootstrap/app.php')
+    ->handleCommand(new ArgvInput);
+EOF
             ],
             [
                 '<?php'.PHP_EOL."ini_set('display_errors', '1');".PHP_EOL.'error_reporting(E_ALL);'.PHP_EOL,
                 "\$app = require_once __DIR__.'/bootstrap/app.php';".PHP_EOL.'$app->useStoragePath(Laravel\Vapor\Runtime\StorageDirectories::PATH);'.PHP_EOL,
                 Manifest::shouldSeparateVendor($this->environment) ? "require '/tmp/vendor/autoload.php';".PHP_EOL : "require __DIR__.'/vendor/autoload.php';".PHP_EOL,
+                <<<EOF
+// Bootstrap Laravel...
+\$app = require_once __DIR__.'/bootstrap/app.php';
+\$app->useStoragePath(Laravel\Vapor\Runtime\StorageDirectories::PATH);
+
+// Handle the command...
+\$status = \$app->handleCommand(new ArgvInput);
+EOF
             ],
             file_get_contents($file)
         );
