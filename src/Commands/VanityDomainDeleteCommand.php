04b5c1964465b2245f27bb141a4a461a1c29da73
Orkhan Ahmadov <orkhan@fastmail.com>
2024-12-13 20:19:27 +0100

    [1.x] Allow force deleting vanity domain without confirmation (#258)
    * Allow force deleting vanity domain without confirmation
    
    * missed import

--- src/Commands/VanityDomainDeleteCommand.php
+++ src/Commands/VanityDomainDeleteCommand.php
@@ -5,6 +5,7 @@ namespace Laravel\VaporCli\Commands;
 use Laravel\VaporCli\Helpers;
 use Laravel\VaporCli\Manifest;
 use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputOption;
 
 class VanityDomainDeleteCommand extends Command
 {
@@ -18,6 +19,7 @@ class VanityDomainDeleteCommand extends Command
         $this
             ->setName('vanity-domain:delete')
             ->addArgument('environment', InputArgument::REQUIRED, 'The environment name')
+            ->addOption('force', null, InputOption::VALUE_NONE, 'Force deletion of the vanity domain without confirmation')
             ->setDescription('Delete the vanity domain associated with the given environment');
     }
 
@@ -29,8 +31,9 @@ class VanityDomainDeleteCommand extends Command
     public function handle()
     {
         $environment = $this->argument('environment');
+        $forceDeletion = $this->option('force');
 
-        if (! Helpers::confirm("Are you sure you want to delete the vanity domain of the [{$environment}] environment", false)) {
+        if (! $forceDeletion && ! Helpers::confirm("Are you sure you want to delete the vanity domain of the [{$environment}] environment", false)) {
             Helpers::abort('Action cancelled.');
         }
 
